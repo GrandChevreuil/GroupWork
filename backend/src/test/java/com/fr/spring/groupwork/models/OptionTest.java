38c468f130bcdde1b3d99418b365398014449c1e
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-07-04 12:23:12 +0200

    test : option

---
+++ backend/src/test/java/com/fr/spring/groupwork/models/OptionTest.java
@@ -0,0 +1,89 @@
+package com.fr.spring.groupwork.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.fr.spring.groupwork.models.enums.EOption;
+
+/**
+ * File : OptionTest.java
+ * Tests unitaires pour la classe Option
+ * @author Mathis Mauprivez
+ * @date 04/07/2025
+ */
+class OptionTest {
+
+    private Option option;
+    
+    @BeforeEach
+    void setUp() {
+        option = new Option();
+    }
+    
+    @Test
+    void defaultConstructor_shouldCreateEmptyOption() {
+        
+        assertThat(option.getId()).isNull();
+        assertThat(option.getName()).isNull();
+    }
+    
+    @Test
+    void constructorWithName_shouldSetName() {
+        // Arrange & Act
+        EOption optionName = EOption.COMPUTER_SCIENCE;
+        Option optionWithName = new Option(optionName);
+        
+        
+        assertThat(optionWithName.getName()).isEqualTo(optionName);
+    }
+    
+    @Test
+    void setAndGetId_shouldWorkCorrectly() {
+        
+        Integer id = 5;
+        
+        
+        option.setId(id);
+        
+        
+        assertThat(option.getId()).isEqualTo(id);
+    }
+    
+    @Test
+    void setAndGetName_shouldWorkCorrectly() {
+        
+        EOption name = EOption.MATHEMATICS;
+        
+        
+        option.setName(name);
+        
+        
+        assertThat(option.getName()).isEqualTo(name);
+    }
+    
+    @Test
+    void idShouldBeNullByDefault() {
+        
+        Option newOption = new Option();
+        assertThat(newOption.getId()).isNull();
+    }
+    
+    @Test
+    void nameShouldBeNullInDefaultConstructor() {
+        
+        Option newOption = new Option();
+        assertThat(newOption.getName()).isNull();
+    }
+    
+    @Test
+    void constructorWithName_shouldNotSetId() {
+        // Arrange & Act
+        EOption optionName = EOption.COMPUTER_SCIENCE;
+        Option optionWithName = new Option(optionName);
+        
+        
+        assertThat(optionWithName.getId()).isNull();
+    }
+}
