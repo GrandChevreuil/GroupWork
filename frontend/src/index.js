f2b21ee67298a1f33ebd6fcdede691115abf409a
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-07-08 08:09:02 +0200

    fix : inversion ordre import pour éviter écrasement de style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,16 @@
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
-import '@mantine/core/styles.css';
+import './index.css';                           // Move your Tailwind reset first
+import '@mantine/core/styles.css';              // Then Mantine styles
 import '@mantine/notifications/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/dropzone/styles.css';
 import '@mantine/dates/styles.css';
 import '@mantine/code-highlight/styles.css';
 
-import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+
 import App from './App';
 import { theme } from './theme';
 
